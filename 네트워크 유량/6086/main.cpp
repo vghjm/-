e9bcba5788b8209b88eb753b507457f3e811b116
vghjm <52833902+vghjm@users.noreply.github.com>
2019-09-11 19:57:28 +0900

    Add files via upload

---
+++ 네트워크 유량/6086/main.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+const int MAX_V = 52;
+const int INF = 1000000000;
+ 
+inline int CtoI(char c){
+    if(c <= 'Z') return c - 'A';
+    return c - 'a' + 26;
+}
+ 
+int main(){
+    int N;
+    int c[MAX_V][MAX_V] = {0};
+    int f[MAX_V][MAX_V] = {0};
+    vector<int> adj[MAX_V];
+ 
+    scanf("%d", &N);
+    while(N--){
+        char u, v;
+        int w;
+        scanf(" %c %c %d", &u, &v, &w);
+        u = CtoI(u); v = CtoI(v);
+        c[u][v] = c[v][u] += w;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+ 
+    int total = 0, S = CtoI('A'), E = CtoI('Z');
+    while(1){
+        int prev[MAX_V];
+        fill(prev, prev+MAX_V, -1);
+        queue<int> Q;
+        Q.push(S);
+        while(!Q.empty() && prev[E] == -1){
+            int curr = Q.front();
+            Q.pop();
+            for(int next: adj[curr]){
+                if(c[curr][next]-f[curr][next] > 0 && prev[next] == -1){
+                    Q.push(next);
+                    prev[next] = curr;
+                    if(next == E) break;
+                }
+            }
+        }
+        if(prev[E] == -1) break;
+ 
+        int flow = INF;
+        for(int i=E; i!=S; i=prev[i])
+            flow = min(flow, c[prev[i]][i]-f[prev[i]][i]);
+        for(int i=E; i!=S; i=prev[i]){
+            f[prev[i]][i] += flow;
+            f[i][prev[i]] -= flow;
+        }
+        total += flow;
+    }
+    
+    printf("%d\n", total);
+}
+
+
