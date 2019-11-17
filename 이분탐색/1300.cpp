13f5d1cc82d911fc328f32d8fe3f0928cb7be900
배성준 <52833902+vghjm@users.noreply.github.com>
2019-11-17 20:00:28 +0900

    Add files via upload

---
+++ 이분탐색/1300.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    int N, K;
+    cin >> N >> K;
+    int low = 1, high = K;
+    int result = -1;
+
+    while(low <= high){
+        int cnt = 0;
+        int mid = (low + high) / 2;
+        for (int i = 1; i <= N; i++)
+            cnt += min(mid / i, N); //mid보다 작은 j의 수(i * j <= mid)
+        if (cnt < K)
+            low = mid + 1;
+        else{
+            result = mid;
+            high = mid - 1;
+        }
+    }
+    cout << result;
+
+    return 0;
+
+}
